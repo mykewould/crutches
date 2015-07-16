db8b43d1aa23bcfc511005e755592ffdd6c0a443
nawns <nonis@protonmail.com>
2015-07-16 18:09:10 -0500

    4 - List.without

---
+++ lib/crutches/list.ex
@@ -0,0 +1,23 @@
+defmodule Crutches.List do
+  @type t :: List
+  @doc ~S"""
+  Returns a copy of the List without the specified elements.
+  
+  ## Examples
+     
+      iex> List.without(["David", "Rafael"], ["David"])
+      ["Rafael"]
+
+      iex> List.without(["David", "Rafael", "Aaron", "Todd"], ["Aaron", "Todd"])
+      ["David", "Rafael"]
+
+      iex> List.without([1, 1, 2, 1, 4], [1, 2])
+      [4]
+  
+  """
+  @spec without(t, t) :: t
+  def without(collection, elements) do
+    Enum.filter(collection, fn(x) -> !Enum.member?(elements, x) end)
+  end
+
+end
