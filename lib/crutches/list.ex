b235ed963a76725772057da2481eb415be971882
Michael Wood <mykewould@gmail.com>
2015-07-20 17:25:47 -0700

    Fix params order

--- lib/crutches/list.ex
+++ lib/crutches/list.ex
@@ -133,10 +133,10 @@ defmodule Crutches.List do
                       |> Crutches.List.shorten(1)
                       |> Enum.join(merged_opts[:words_connector])
     end
-    connect_sentence(start_of, List.last(words), connector)
+    connect_sentence(start_of, connector, List.last(words))
   end
 
-  defp connect_sentence(start_of, last_word, connector) do
+  defp connect_sentence(start_of, connector, last_word) do
     "#{start_of}#{connector}#{last_word}"
   end
 
