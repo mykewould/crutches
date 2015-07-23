1a791d75f4b6945b59eed752295388c38b7a77f6
Kash Nouroozi <knouroozi@gmail.com>
2015-07-23 17:52:17 -0400

    Initialize List.to_sentence accepted_options and default_connectors only once on module compilation instead of whenever the List.to_sentence function is called #optimization

--- lib/crutches/list.ex
+++ lib/crutches/list.ex
@@ -100,44 +100,50 @@ defmodule Crutches.List do
   def to_sentence(words, options \\ [])
   def to_sentence([],     _), do: ""
   def to_sentence([word], _), do: "#{word}"
-  def to_sentence(words, options) do
-    bad_options_check(options)
+  def to_sentence(words, provided_options) do
+    bad_options_check(provided_options)
 
-    merged_opts = merge_default_options(options)
-    start_of    = words
-                  |> Crutches.List.shorten
-                  |> Enum.join(merged_opts[:words_connector])
+    options = merge_default_options(provided_options)
+    start_of = words
+      |> Crutches.List.shorten
+      |> Enum.join(options[:words_connector])
 
     case length(words) do
-      2 -> connector = merged_opts[:two_words_connector]
-      _ -> connector = merged_opts[:last_word_connector]
+      2 -> connector = options[:two_words_connector]
+      _ -> connector = options[:last_word_connector]
     end
 
     "#{start_of}#{connector}#{List.last(words)}"
   end
 
-  defp merge_default_options(options) do
-    default_connectors = [
-      words_connector:      ", ",
-      two_words_connector:  " and ",
-      last_word_connector:  ", and "
+  @to_sentence [
+    accepted_options: ~w( words_connector
+                          two_words_connector
+                          last_word_connector
+                          locale )a,
+    default_connectors: [
+      words_connector: ", ",
+      two_words_connector: " and ",
+      last_word_connector: ", and "
     ]
+  ]
 
-    new_options = Keyword.merge(default_connectors, options)
+  defp merge_default_options(options) do
+    new_options = @to_sentence[:default_connectors] |> Keyword.merge(options)
     if new_options[:locale] do
-      new_options = Keyword.merge(new_options, options[:locale][:support][:array])
+      new_options |> Keyword.merge(options[:locale][:support][:array])
+    else
+      new_options
     end
-    new_options
   end
 
   defp bad_options_check(options) do
-    good_opts = ~w( words_connector
-                    two_words_connector
-                    last_word_connector
-                    locale )a
-    bad_opts  = Enum.reject(Keyword.keys(options), &Enum.member?(good_opts, &1))
-    if length(bad_opts) > 0 do
-      raise ArgumentError, message: "Unknown key #{hd(bad_opts)}"
+    bad_options =
+      options
+      |> Keyword.keys
+      |> Enum.reject(&Enum.member?(@to_sentence[:accepted_options], &1))
+    if Enum.any?(bad_options) do
+      raise ArgumentError, message: "Unknown key #{hd(bad_options)}"
     end
   end
 
