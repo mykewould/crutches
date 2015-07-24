0e521fb0909aedc5b00507324aea08061c56c038
Kash Nouroozi <knouroozi@gmail.com>
2015-07-24 16:00:26 -0400

    Added Crutches.Monad :wink:

---
+++ lib/crutches/monad.ex
@@ -0,0 +1,32 @@
+defmodule Crutches.Monad do
+  defmacro __using__(_opts) do
+    quote do
+      require Crutches.Maybe
+      require Crutches.Result
+      alias Crutches.Maybe
+      alias Crutches.Result
+
+      import Crutches.Monad
+    end
+  end
+
+  defmacro module(type) do
+    cond do
+      type == :some || type == :none -> Crutches.Maybe
+      type == :ok || type == :error  -> Crutches.Result
+      true -> Enum
+    end
+  end
+
+  def join(monad = {type, _}) do
+    apply module(type), :join, [monad]
+  end
+
+  def map(monad = {type, _}, function) do
+    apply module(type), :map, [monad, function]
+  end
+
+  def chain(monad = {type, _}, function) do
+    apply module(type), :chain, [monad, function]
+  end
+end
