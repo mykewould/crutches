c15a0a6957e384083cfbd8135fa645b9e52fb74e
Kash Nouroozi <knouroozi@gmail.com>
2015-07-24 15:59:44 -0400

    Cleaning up.

--- lib/crutches/maybe.ex
+++ lib/crutches/maybe.ex
@@ -1,6 +1,5 @@
 defmodule Crutches.Maybe do
   def of(value) when is_nil(value), do: none
-  def of({:none, _}), do: none
   def of(value), do: some(value)
 
   def none, do: {:none, nil}
@@ -17,16 +16,16 @@ defmodule Crutches.Maybe do
     quote do: elem(unquote(maybe), 0) == :some
   end
 
-  def join(maybe) when none?(maybe), do: none
+  def join({:none, _}), do: none
   def join({:some, {:some, value}}), do: join({:some, value})
   def join({:some, value}), do: {:some, value}
 
-  def map({:none, _}, function) when is_function(function), do: none
+  def map({:none, _}, _), do: none
   def map({:some, value}, function) when is_function(function) do
     value |> function.() |> of
   end
 
-  def chain({:none, _}, function) when is_function(function), do: none
+  def chain({:none, _}, _), do: none
   def chain({:some, value}, function) when is_function(function) do
     value |> function.() |> join
   end
