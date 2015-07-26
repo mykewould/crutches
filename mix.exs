ac92ede8ff57278ea3f7a4affa41235b768fee6c
Michael Wood <mykewould@gmail.com>
2015-07-25 23:10:21 -0700

    Update version 0.0.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Crutches.Mixfile do
 
   def project do
     [app: :crutches,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
