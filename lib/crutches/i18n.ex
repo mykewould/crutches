106de90176c1e6f020cf0051609e940eb22dc6f8
Kash Nouroozi <knouroozi@gmail.com>
2015-07-29 12:55:33 -0400

    Basic loading of I18n yaml files

--- lib/crutches/i18n.ex
+++ lib/crutches/i18n.ex
@@ -1,2 +1,10 @@
 defmodule Crutches.I18n do
+  use Linguist.Vocabulary
+
+  def load(locale) when is_binary(locale) do
+    [document|_] =
+      Path.join([__DIR__, "..", "..", "config", "locale", "#{locale}.yml"])
+      |> :yamerl_constr.file
+    :proplists.get_value(String.to_char_list(locale), document)
+  end
 end
