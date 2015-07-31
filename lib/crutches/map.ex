c095e246fdf3ce1741fddef0423e261ecf33c4d3
Michael Wood <mykewould@gmail.com>
2015-07-31 11:08:47 -0500

    name change and refactor

--- lib/crutches/map.ex
+++ lib/crutches/map.ex
@@ -2,35 +2,26 @@ defmodule Crutches.Map do
   @doc ~S"""
   Recursively traverse a (nested) hash and change the keys based on
   the function provided.
-
+  
   ## Examples
-
+  
   iex> map = %{"hello" => %{"goodbye" => 1}, "akuna" => "matata"}
-  iex> Map.deep_key_change(map, fn (key) -> String.to_atom(key) end)
+  iex> Map.dkey_update(map, fn (key) -> String.to_atom(key) end)
   %{:hello => %{:goodbye => 1}, :akuna => "matata"}
-
+  
   iex> map = %{"hello" => %{"goodbye" => 1, "akuna" => "matata", "hello" => %{"goodbye" => 1, "akuna" => "matata"}}, "akuna" => "matata"}
-  iex> Map.deep_key_change(map, fn (key) -> String.to_atom(key) end)
+  iex> Map.dkey_update(map, fn (key) -> String.to_atom(key) end)
   %{hello: %{akuna: "matata", goodbye: 1, hello: %{akuna: "matata", goodbye: 1}}, akuna: "matata"}
-
+  
   """
-  def deep_key_change(map, fun) do
-    deep_key_change(map, fun, %{})
-  end
-
-  def deep_key_change(map, _, acc) when map == %{} do
-    acc
-  end
-
-  def deep_key_change(map, fun, acc) do
+  def dkey_update(map, fun), do: dkey_update(map, fun, %{})
+  def dkey_update(map, _, acc) when map == %{}, do: acc
+  def dkey_update(map, fun, acc) do
     key = Map.keys(map) |> List.first
-    if is_map(map[key]) do
-      value = deep_key_change(map[key], fun, %{})
-    else
-      value = map[key]
+    case is_map(map[key]) do
+      true -> value = dkey_update(map[key], fun)
+         _ -> value = map[key]
     end
-    acc = Map.put(acc, fun.(key), value)
-    map = Map.delete(map, key)
-    deep_key_change(map, fun, acc)
+    dkey_update(Map.delete(map, key), fun, Map.put(acc, fun.(key), value))
   end
 end
