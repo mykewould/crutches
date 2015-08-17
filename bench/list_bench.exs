ad2875401b34506847da499f25294795678c5749
Kash Nouroozi <knouroozi@gmail.com>
2015-08-16 22:33:38 -0400

    Added a bench for List.split

--- bench/list_bench.exs
+++ bench/list_bench.exs
@@ -1,9 +1,14 @@
 defmodule Crutches.ListBench do
+  alias Crutches.List
   use Benchfella
 
   @list 1..2000 |> Enum.to_list
 
   bench "shorten/2 2000 by 1500" do
-    Crutches.List.shorten @list, 1500
+    List.shorten @list, 1500
+  end
+
+  bench "split/2 2000 on evens" do
+    List.split @list, &(rem(&1, 2) == 0)
   end
 end
