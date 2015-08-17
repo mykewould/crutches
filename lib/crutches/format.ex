dbb137027144d00848c489f0bf2f2f52076913ed
Kash Nouroozi <knouroozi@gmail.com>
2015-08-16 23:46:51 -0400

    Moved Integer.ordinal/ordinalize to Format.Integer (#70)

--- lib/crutches/format.ex
+++ lib/crutches/format.ex
@@ -1,8 +1,11 @@
 defmodule Crutches.Format do
   defmacro __using__(_opts) do
     quote do
-      alias Crutches.Format.List
-      alias Crutches.Format.Number
+      alias Crutches.Format
+
+      alias Format.List
+      alias Format.Number
+      alias Format.Integer
     end
   end
 end
---
+++ test/crutches/format/integer_test.exs
@@ -0,0 +1,5 @@
+defmodule Crutches.Format.IntegerTest do
+  alias Crutches.Format.Integer
+  use ExUnit.Case, async: true
+  doctest Crutches.Format.Integer
+end
