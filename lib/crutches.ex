03815a0023da8ebbb459d14ee40062aba8413435
Kash Nouroozi <knouroozi@gmail.com>
2015-08-16 23:43:00 -0400

    Added __using__ macros to Crutches and Crutches.Format

--- lib/crutches.ex
+++ lib/crutches.ex
@@ -1,2 +1,13 @@
 defmodule Crutches do
+  defmacro __using__(_opts) do
+    quote do
+      alias Crutches.Enum
+      alias Crutches.List
+      alias Crutches.Map
+      alias Crutches.String
+      alias Crutches.Integer
+
+      alias Crutches.Format
+    end
+  end
 end
--- lib/crutches/format.ex
+++ lib/crutches/format.ex
@@ -1,2 +1,8 @@
 defmodule Crutches.Format do
+  defmacro __using__(_opts) do
+    quote do
+      alias Crutches.Format.List
+      alias Crutches.Format.Number
+    end
+  end
 end
