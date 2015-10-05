885d42f0ec2f92aab4c8d3eefbb5806951fc470e
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2015-10-05 16:49:17 +0200

    Remove comments in config.exs and mix.exs

--- mix.exs
+++ mix.exs
@@ -16,22 +16,10 @@ defmodule Crutches.Mixfile do
                links: %{"GitHub" => "https://github.com/mykewould/crutches"}]]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:inch_ex,    only: :docs},
      {:ex_doc,     only: :docs},
