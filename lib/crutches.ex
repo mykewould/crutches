18b6d9c183df9751742a3556c071c5aed97a025e
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2015-10-05 17:47:16 +0200

    Document main module

--- lib/crutches.ex
+++ lib/crutches.ex
@@ -1,4 +1,19 @@
 defmodule Crutches do
+  @moduledoc """
+  Crutches is an addition to the Elixir standard library, inspired by
+  ActiveSupport. We include a number of utilities that we feel are missing in
+  the Elixir standard library.
+
+  Get Crutches by adding the following to your `mix.exs` and afterwards, run
+  `mix deps.get`:
+
+      defp deps do
+        [{:crutches, "`~> 1.0.0`"}]
+      end
+
+  Then, `use Crutches` in a module and find all of our functions and modules
+  under the `C` alias. Alternatively, alias it to something you prefer.
+  """
   defmacro __using__(_opts) do
     quote do
       alias Crutches,        as: C
